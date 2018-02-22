6e6a2563a713edef0123cdc326847ca92dfc9719
Puntiaey <35341443+Puntiaey@users.noreply.github.com>
2018-02-23 01:04:01 +0700

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from .models import Car,Rent
 from .models import Person
 from .forms import PersonForm
+from .forms import CarForm
 
 # Create your views here.
 
 def home(request):
 	return render(request, 'home.html')
 	
-def creates(request):
-	return render(request, 'create.html')
+def list(request):
+	return render(request, 'list.html')
 
 class CreatePersonView(CreateView):
 	queryset = Person()
@@ -20,9 +21,15 @@ class CreatePersonView(CreateView):
 	form_class = PersonForm
 	success_url = '/admin'
 
+class CreateCarView(CreateView):
+	queryset = Car()
+	template_name='rentcar.html'
+	form_class = CarForm
+	success_url = '/admin'
+
 class ListPersonView(ListView):
-    model = Person	
-	
+    model = Person
+
 class CarListView(ListView):
     model = Car
     template_name='car_list.html'
